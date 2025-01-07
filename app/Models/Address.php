0889320524793dfcd04291d5839b6024315f16ea
Henrique Alves <alves@Henriques-MacBook-Pro.local>
2025-01-06 22:10:37 -0500

    feat: add contact and address models

---
+++ app/Models/Address.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    /** @use HasFactory<\Database\Factories\AddressFactory> */
+    use HasFactory;
+
+    protected $fillable = ['cep', 'uf', 'cidade', 'bairro', 'numero', 'complemento', 'latitude', 'longitude'];
+
+    public function contact() {
+        return $this->hasMany(Contact::class);
+    }
+}
---
+++ app/Models/Contact.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    /** @use HasFactory<\Database\Factories\ContactFactory> */
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'name', 'cpf', 'phone', 'address_id'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function address() {
+        return $this->belongsTo(Address::class);
+    }
+}
