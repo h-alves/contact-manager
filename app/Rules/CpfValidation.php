818ee3a371967aa60f80c94455afb34801bd040c
Henrique Alves Semmer <henriqueasemmer@gmail.com>
2025-01-09 08:25:43 -0500

    refactor: make cpf validation into more functions

--- app/Rules/CpfValidation.php
+++ app/Rules/CpfValidation.php
@@ -16,30 +16,54 @@ class CpfValidation implements ValidationRule
     {
         $value = preg_replace('/[^0-9]/', '', $value);
 
-        if (strlen($value) != 11) {
+        if ($this->hasElevenCharacters($value)) {
             $fail("O campo {$attribute} deve ter 11 caracteres");
         }
 
-        if (preg_match('/(\d)\1{10}/', $value)) {
+        if ($this->isSequence($value)) {
             $fail("O campo {$attribute} deve ser válido");
         }
 
+        if ($this->hasIncorrectDigits($value)) {
+            $fail("O campo {$attribute} deve ser válido");
+        }
+    }
+
+    private function hasElevenCharacters(string $value): bool
+    {
+        return strlen($value) == 11;
+    }
+
+    private function isSequence(string $value): bool
+    {
+        return preg_match('/^\d{11}$/', $value);
+    }
+
+    private function calculateFirstDigit($value): int
+    {
         $sum1 = 0;
         for ($i = 0; $i < 9; $i++) {
             $sum1 += (int) $value[$i] * ($i + 1);
         }
         $sum1 %= 11;
-        $sum1 = ($sum1 < 10) ? $sum1 : 0;
+        return ($sum1 < 10) ? $sum1 : 0;
+    }
 
+    private function calculateSecondDigit($value): int
+    {
         $sum2 = 0;
         for ($i = 0; $i < 10; $i++) {
-            $sum2 += (int)$value[$i] * $i;
+            $sum2 += (int) $value[$i] * $i;
         }
         $sum2 %= 11;
-        $sum2 = ($sum2 < 10) ? $sum2 : 0;
+        return ($sum2 < 10) ? $sum2 : 0;
+    }
 
-        if ($sum1 != $value[9] || $sum2 != $value[10]) {
-            $fail("O campo {$attribute} deve ser válido");
-        }
+    private function hasIncorrectDigits($value): bool
+    {
+        $firstDigit = $this->calculateFirstDigit($value);
+        $secondDigit = $this->calculateSecondDigit($value);
+
+        return $firstDigit != $value[9] || $secondDigit != $value[10];
     }
 }
