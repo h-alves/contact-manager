d31900a3aa968c89d106cacf48094415c567b50f
Henrique Alves Semmer <henriqueasemmer@gmail.com>
2025-01-09 15:43:35 -0500

    fix: removing feature test

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,7 @@ use App\Http\Controllers\AddressController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PasswordResetController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
